6edc5a4e035f78da127e4fb4706d3dd0ce42bc39
yingzheng <870428198@qq.com>
2020-07-24 15:01:36 +0800

    第一次提交

---
+++ polls/views.py
@@ -0,0 +1,37 @@
+import os
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from mysite.settings import BASE_DIR
+from .models import Question
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def hello(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+
+def printPath(request):
+    return HttpResponse(os.path.join(BASE_DIR, "polls\\html"))
